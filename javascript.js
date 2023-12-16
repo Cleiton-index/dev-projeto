7aef3b70ed70c11d57a4d84fe3b53e16f179e2bf
Cleiton <cleitonarguelhodasilva2@gmail.com>
2023-12-15 22:27:51 -0400

    modificação id, minutes.

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-const minutesEl = document.querySelector("#minutes")
-const secondsEl = document.querySelector("#seconds")
-const millisecondsEl = document.querySelector("#milliseconds")
-const startBtn = document.querySelector("#startBtn")
-const pauseBtn = document.querySelector("#pauseBtn")
-const resumeBtn = document.querySelector("#resumeBtn")
-const resetBtn = document.querySelector("#resetBtn")
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
 let interval;
 let minutes = 0;
@@ -18,61 +18,56 @@ resumeBtn.addEventListener("click", resumeTimer);
 resetBtn.addEventListener("click", resetTimer);
 
 function startTimer() {
-    interval = setInterval(() => {
-        if (!isPaused)
-            milliseconds += 10;
-
-        if (milliseconds === 1000) {
-            seconds++;
-            milliseconds = 0;
-        }
-        if (seconds === 60) {
-            minutes++;
-            seconds = 0;
-        }
-        minutesEl.textContent = formatTime(minutes);
-        secondsEl.textContent = formatTime(seconds);
-        millisecondsEl.textContent = formatMilliseconds(milliseconds);
-
-
-    }, 10);
-
-
-
-    startBtn.style.display = "none";
-    pauseBtn.style.display = "block";
+  interval = setInterval(() => {
+    if (!isPaused) {
+      milliseconds += 10;
+      if (milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
+      }
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+      minutesEl.innerHTML = formatTime(minutes);
+      secondsEl.innerHTML = formatTime(seconds);
+      millisecondsEl.innerHTML = formatMilliseconds(milliseconds);
+    }
+  }, 10);
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
 }
+
 function pauseTimer() {
-    isPaused = true
-    pauseBtn.style.display = "none";
-    resumeBtn.style.display = "block"
+  isPaused = true;
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "inline-block";
 }
+
 function resumeTimer() {
-    isPaused = false
-    pauseBtn.style.display = "block"
-    resumeBtn.style.display = "none"
+  isPaused = false;
+  resumeBtn.style.display = "none";
+  pauseBtn.style.display = "inline-block";
 }
+
 function resetTimer() {
-    clearInterval(interval);
-    minutes = 0;
-    seconds = 0;
-    milliseconds = 0;
-
-    minutesEl.textContent = "00"
-    secondsEl.textContent = "00"
-    millisecondsEl.textContent = "000"
-
-    startBtn.style.display = "block";
-    pauseBtn.style.display = "none";
-    resetBtn.style.display = "none";
-
-
+  clearInterval(interval);
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+  isPaused = false;
+  minutesEl.innerHTML = "00";
+  secondsEl.innerHTML = "00";
+  millisecondsEl.innerHTML = "000";
+  startBtn.style.display = "inline-block";
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "none";
 }
-
 
 function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
+  return time < 10 ? `0${time}` : time;
 }
+
 function formatMilliseconds(time) {
-    return time < 100 ? `${time}`.padStart(3, "0") : time;
+  return time < 100 ? `0${time}`.padStart(3, "0") : time;
 }
